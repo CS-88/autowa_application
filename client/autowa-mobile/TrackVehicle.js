1e9025fe8b44e7cd185b45ead58ee879dc308080
Sultan-2374 <sulmoulana@gmail.com>
2024-03-11 13:48:31 +0530

    added track vehicle page to mobile application

---
+++ client/autowa-mobile/TrackVehicle.js
@@ -0,0 +1,82 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+export default function TrackVehicle() {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'rgb(255,255,255)', padding: 20 }}>
+      <View>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 35,
+            fontWeight: 'bold',
+            paddingTop: 120,
+          }}>
+          Vehicle Progress
+        </Text>
+      </View>
+      <View style={{ alignItems: 'center', paddingTop: 50 }}>
+        <Image
+          style={{ height: 162, width: 162 }}
+          source={require('../Images/Car_wash.png')}
+        />
+      </View>
+      <View
+        style={{
+          marginTop: '20%',
+          marginLeft: '6%',
+          marginRight: '6%',
+          height: 40,
+          backgroundColor: 'white',
+          borderRadius: 30,
+          borderWidth: 5,
+          borderColor: 'rgb(176, 216, 218)',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            position: 'absolute',
+            width: '25%',
+            height: 30,
+            backgroundColor: 'rgb(176, 216, 218)',
+            borderRadius: 15,
+          }}></View>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 19,
+            fontWeight: 'bold',
+          }}>
+          25%
+        </Text>
+      </View>
+
+      <View style={{ paddingTop: '9%' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+          Your Honda Fit 2020 is currently in Wash
+        </Text>
+      </View>
+      <View style={{ paddingTop: '9%' }}>
+        <Text style={{ fontSize: 17 }}>Lot No : 7</Text>
+      </View>
+      <View>
+        <Text style={{ fontSize: 17, flexDirection: 'row' }}>
+          Attendant : Mr. Nimal
+        </Text>
+        <View style={{ alignItems: 'center', paddingTop: '10%' }}>
+          <TouchableOpacity
+            style={{
+              height: 40,
+              width: 100,
+              backgroundColor: 'gray',
+              borderRadius: 30,
+              justifyContent: 'center',
+            }}>
+            <Text style={{ color: 'white', textAlign: 'center' }}>
+              Live feed 
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
